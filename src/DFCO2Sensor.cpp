9fb5c9ceb3282501688eed20a1425063cf5e9f95
Sam Groveman <samgroveman@outlook.com>
2024-10-15 18:25:56 -0400

    Updated to be consistent with best practices

--- src/DFCO2Sensor.cpp
+++ src/DFCO2Sensor.cpp
@@ -1,48 +1,76 @@
 #include "DFCO2Sensor.h"
 
-/// @brief Starts the CO2 sensor
+/// @brief Creates a Bosch BME280 object using hardware serial
+/// @param SerialPort Pointer to the serial port to use
 /// @param RX_Pin The RX pin to use
 /// @param TX_Pin The TX pin to use
-/// @return True on success
-bool DFCO2Sensor::begin(int RX_Pin, int TX_Pin) {
-	Serial2.begin(9600, SERIAL_8N1, RX_Pin, TX_Pin);
-	values.resize(1);
+DFCO2Sensor::DFCO2Sensor(HardwareSerial* SerialPort, int RX_Pin, int TX_Pin) {
+	rxpin = RX_Pin;
+	txpin = TX_Pin;
+	hardwareSerialPort = SerialPort;
+}
+
+/// @brief Creates a Bosch BME280 object using software serial
+/// @param RX_Pin The RX pin to use
+/// @param TX_Pin The TX pin to use
+DFCO2Sensor::DFCO2Sensor(int RX_Pin, int TX_Pin) : softwareSerialPort(RX_Pin, TX_Pin) {
+	use_soft_serial = true;
+}
+
+bool DFCO2Sensor::begin() {
 	Description.parameterQuantity = 1;
 	Description.type = "Environmental Sensor";
 	Description.name = "CO2 Sensor";
 	Description.parameters = {"CO2"};
 	Description.units = {"ppm"};
 	Description.id = 3;
-	bool result = false;
+	values.resize(Description.parameterQuantity);
+	if (use_soft_serial) {
+		softwareSerialPort.begin(9600);
+	} else 
+	{
+		hardwareSerialPort->begin(9600, SERIAL_8N1, rxpin, txpin);
+	}
 	return true;
 }
 
-/// @brief Take a CO2 measurement and stores it in an internal variable
-/// @return True on success
 bool DFCO2Sensor::takeMeasurement() {
 	// Send command to take measurement
-	Serial2.write(readCO2Data, 9);
+	if(use_soft_serial) {
+		softwareSerialPort.write(readCO2Data, 9);
+	} else {
+		hardwareSerialPort->write(readCO2Data, 9);
+	}
 
 	// Wait for measurement to be available
-	vTaskDelay(500 / portTICK_PERIOD_MS);
+	delay(500);
 
 	// Read measurement from serial
 	for (int i = 0, j = 0; i < 9; i++)
 	{
-		if (Serial2.available() > 0)
-		{
-			uint high, low;
-			int ch = Serial2.read();
-			Serial1.println(ch);
-			if (i == 2) {
-				high = ch;    // High concentration
+		uint high, low;
+		int ch;
+		if (use_soft_serial) {
+			if (softwareSerialPort.available() > 0)
+			{
+				uint high, low;
+				ch = softwareSerialPort.read();
 			}
-			if (i == 3) {
-				low = ch;    // Low concentration
+		} else {
+			if (hardwareSerialPort->available() > 0)
+			{
+				uint high, low;
+				ch = hardwareSerialPort->read();
 			}
-			if (i == 8) {
-				values[0] = high * 256 + low; // CO2 concentration
-			}
+		}
+		if (i == 2) {
+			high = ch;    // High concentration
+		}
+		if (i == 3) {
+			low = ch;    // Low concentration
+		}
+		if (i == 8) {
+			values[0] = high * 256 + low; // CO2 concentration
 		}
 	}
 }
--- src/DFCO2Sensor.h
+++ src/DFCO2Sensor.h
@@ -1,6 +1,9 @@
 /*
 * This file and associated .cpp file are licensed under the GPLv3 License Copyright (c) 2024 Sam Groveman
 *
+* External libraries needed:
+* ESPSoftwareSerial: https://github.com/plerup/espsoftwareserial
+*
 * CO2 Sensor: https://www.dfrobot.com/product-1565.html
 *
 * Contributors: Sam Groveman
@@ -9,13 +12,32 @@
 #pragma once
 #include <Arduino.h>
 #include <Sensor.h>
+#include <SoftwareSerial.h>
+#include <HardwareSerial.h>
 
 class DFCO2Sensor: public Sensor {
-	protected:		
-		bool begin(int RX_Pin = 16, int TX_Pin = 17);
+	protected:
+		DFCO2Sensor(HardwareSerial* SerialPort, int RX_Pin = 16, int TX_Pin = 17);
+		DFCO2Sensor(int RX_Pin, int TX_pin);
+		bool begin();
 		bool takeMeasurement();
 
 	protected:
 		/// @brief Command to read CO2 data from sensor.
 		const unsigned char readCO2Data[9] = { 0xFF, 0x01, 0x86, 0x00, 0x00, 0x00, 0x00, 0x00, 0x79 };
+
+		/// @brief Pointer to hardware serial port, if used
+		HardwareSerial* hardwareSerialPort;
+
+		/// @brief SoftwareSerial port, if using
+		SoftwareSerial softwareSerialPort;
+
+		/// @brief Indicates if software serial is being used
+		bool use_soft_serial = false;
+
+		/// @brief The RX pin used
+		int rxpin;
+
+		/// @brief The TX pin used
+		int txpin;
 };
